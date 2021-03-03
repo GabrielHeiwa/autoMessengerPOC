3b00af922fd1e9a30ee6eb3461eb0675bf89a502
gabrielheiwa <heiwa.gabriel@gmail.com>
2021-03-03 10:33:30 -0300

    try

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var whatsapp_web_js_1 = require("whatsapp-web.js");
+var path_1 = require("path");
+var a = 1;
+var pathChrome = a === 0
+    ? path_1.resolve(__dirname, "node_modules", "puppeteer", ".local-chromium", "linux-856583", "chrome-linux", "chrome")
+    : "C:\\Users\\heiwa\\Documents\\programming\\Self-Corie-POC\\node_modules\\puppeteer\\.local-chromium\\win64-818858\\chrome-win\\chrome.exe";
 var web = new whatsapp_web_js_1.Client({
     puppeteer: {
-        product: "chrome",
+        executablePath: pathChrome,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
 });
 web.on("qr", function (qr) {
     console.log(qr + "\n");
+    console.log(web.pupBrowser);
 });
 web.initialize();
--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
 import { Client } from "whatsapp-web.js";
-
+import { resolve } from "path";
+let a = 0;
+let pathChrome = a === 0
+    ? resolve(__dirname, "node_modules", "puppeteer", ".local-chromium", "linux-856583", "chrome-linux", "chrome")
+    : "C:\\Users\\heiwa\\Documents\\programming\\Self-Corie-POC\\node_modules\\puppeteer\\.local-chromium\\win64-818858\\chrome-win\\chrome.exe"
 const web = new Client({
     puppeteer: {
-        product: "chrome",
+        executablePath: pathChrome,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
-
 });
+
+
 
 web.on("qr", qr => {
     console.log(`${qr}\n`);
+    console.log(web.pupBrowser)
 })
 
 
