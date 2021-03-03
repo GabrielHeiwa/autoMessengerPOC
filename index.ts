d370bbbf0580e1640988f30a98b62b31c658e4b3
gabrielheiwa <heiwa.gabriel@gmail.com>
2021-03-03 10:45:16 -0300

    try

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import { Client } from "whatsapp-web.js";
 import { resolve } from "path";
-let a = 0;
+let a = 1;
 let pathChrome = a === 0
     ? resolve(__dirname, "node_modules", "puppeteer", ".local-chromium", "linux-856583", "chrome-linux", "chrome")
     : "C:\\Users\\heiwa\\Documents\\programming\\Self-Corie-POC\\node_modules\\puppeteer\\.local-chromium\\win64-818858\\chrome-win\\chrome.exe"
 const web = new Client({
     puppeteer: {
-        executablePath: pathChrome,
+        // executablePath: pathChrome,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
 });
